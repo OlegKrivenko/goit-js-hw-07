9c5937a80c1a8d364ec4c0be5de6156028440957
OlegKrivenko <krivenko.olegus@gmail.com>
2023-02-18 12:31:20 +0200

    done HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  gallery: document.querySelector(".gallery"),
+};
+
+ref.gallery.insertAdjacentHTML("beforeend", createGalleryItems(galleryItems));
+ref.gallery.addEventListener("click", getBigImgUrl);
+
+function createGalleryItems(arrayGalleryItems) {
+  return arrayGalleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image"
+          src="${preview}" 
+          data-source="${original}" 
+          alt="${description}" 
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+function getBigImgUrl(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  //      початок модального вікна з великою фоткою basicLightbox
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} width="800" height="600">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleKeydown);
+        console.log("addEventListener");
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleKeydown);
+        console.log("removeEventListener");
+      },
+    }
+  );
+  instance.show();
+
+  function handleKeydown(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  //     кінець модального вікна
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ref = {
+  gallery: document.querySelector(".gallery"),
+};
+
+ref.gallery.insertAdjacentHTML("beforeend", createGalleryItems(galleryItems));
+ref.gallery.addEventListener("click", getBigImgUrl);
+
+function createGalleryItems(arrayGalleryItems) {
+  return arrayGalleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+              </a>`;
+    })
+    .join("");
+}
+
+function getBigImgUrl(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  //      модальне вікно з великою фоткою SimpleLightbox
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    //  fadeSpeed: 150,
+  });
+  //      модальне вікно з великою фоткою SimpleLightbox
+}
